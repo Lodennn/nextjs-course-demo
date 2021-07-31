bd914eec5c132c449d65ec13ff16df09bce80941
Lodenn <khalednasser788@gmail.com>
2021-07-31 04:57:47 +0200

    fix fallback and ignore .next

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ const HomePage = (props) => {
           content="Browse a huge list of highly active meetups"
         />
       </Head>
-      <MeetupList meetups={props.meetups} />;
+      <MeetupList meetups={props.meetups} />
     </Fragment>
   );
 };
